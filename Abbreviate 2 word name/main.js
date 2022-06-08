47d8cca4b480c14f28448c533ec3bda7b458ec2e
ErinUpton <e.upton.softwareeng@gmail.com>
2022-06-07 21:28:55 -0700

    [Add New Soln] `abbrevName` takes first and last name and returns initials

---
+++ Abbreviate 2 word name/main.js
@@ -0,0 +1,17 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+function abbrevName(name){
+
+  name = name.split(' ');
+  let initials = `${name[0].slice(0, 1)}.${name[1].slice(0, 1)}`
+
+  return initials.toUpperCase();
+}
+
+console.log(abbrevName('Erin Upton'))
