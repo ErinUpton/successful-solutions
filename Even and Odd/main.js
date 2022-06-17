31c3d9bc1c1a0732e70f574720696fad6a7d929c
ErinUpton <e.upton.softwareeng@gmail.com>
2022-06-17 09:13:39 -0700

    [new soln] using if...else...

--- Even and Odd/main.js
+++ Even and Odd/main.js
@@ -9,6 +9,7 @@
 
 // In C and JavaScript you should return an array of two elements such as the first is NE and the second is NO.
 
+// Solution 1: ternary 
 function evenAndOdd(num){
     num = num.toString().split('')
     let NE = [];
@@ -26,3 +27,33 @@ function evenAndOdd(num){
   }
 
   console.log(evenAndOdd(126453))
+
+
+// Solution: 2 if... else.. 
+// Pseudo Code 
+// split even numbers NE 
+// % 2 === 0 
+// 0 is even NE 
+// split odd numbers NO 
+// % 2 !== 0 
+
+function splitEvenAndOdd(N) {
+
+  const NE = []
+  const NO = []
+
+  N.toString().split('').forEach(e => {
+
+    if(e % 2 === 0) {
+      NE.push(e)
+    } else if(e % 2 !== 0) {
+      NO.push(e)
+    }
+  })
+
+  return [Number(NE.join('')),  Number(NO.join(''))]
+
+}
+
+
+console.log(splitEvenAndOdd(12345))
